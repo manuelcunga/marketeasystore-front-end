59703938071d6e53c31ea130e892ffbe2d813555
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 12:20:31 +0100

    revert: :recycle: refatorando app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { CreateOrdersComponent } from './create-orders/create-orders.component';
+import { ListOrdersComponent } from './list-orders/list-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    ProductComponent,
+    ListProductsComponent,
+    CreateOrdersComponent,
+    ListOrdersComponent
   ],
   imports: [
     BrowserModule,
