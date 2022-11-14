06a9a1144a3ddea18805349ab245bdd383e8042d
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 16:43:15 +0100

    login feature

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private loginService: LoginService,
+    private router: Router, private notification: NgToastService) { }
 
-  ngOnInit(): void {
+
+  doLogin(form: NgForm){
+    if (form.invalid) {
+      return this.notification.warning({
+        detail: 'warning!',
+        summary: 'Campos obrigatorios!',
+        duration: 2000,
+      });
+    }
+    
+    this.loginService.login(form.value).subscribe(
+      
+      (res)=>{
+        localStorage.setItem('user_token', res.token)
+        sessionStorage.setItem('user_session', res.token)
+        this.router.navigate(['/produto'])
+        
+      },
+      (err)=>{
+         return this.notification.error({
+              detail: 'Erro!',
+              summary: 'Email or password incorrect',
+              duration: 2000,
+            });
+      }
+    )
+
   }
 
+  
 }
