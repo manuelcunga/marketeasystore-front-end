985c021f38f7ffcd85af6a62b5c5e75de7129587
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 12:21:15 +0100

    feat: :sparkles: add create order test

---
+++ src/app/create-orders/create-orders.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateOrdersComponent } from './create-orders.component';
+
+describe('CreateOrdersComponent', () => {
+  let component: CreateOrdersComponent;
+  let fixture: ComponentFixture<CreateOrdersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateOrdersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateOrdersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
