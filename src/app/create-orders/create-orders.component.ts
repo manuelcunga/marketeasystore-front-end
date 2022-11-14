ea7ab3d29d7c3e726aec4287d6488666006e485e
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 12:21:28 +0100

    feat: :sparkles: add create order ts

---
+++ src/app/create-orders/create-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-orders',
+  templateUrl: './create-orders.component.html',
+  styleUrls: ['./create-orders.component.css']
+})
+export class CreateOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
