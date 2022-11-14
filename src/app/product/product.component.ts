8824397e2ceb08177e6e582ed5a9231f4fe6e16d
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 16:56:31 +0100

    update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,7 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor( private notification: NgToastService, private productService: ProductsService) { }
 
   ngOnInit(): void {
+  }
+
+  CreateProduct(form: NgForm){
+    if (form.value == ''){
+      return this.notification.warning({
+        detail: 'Campos vazios!',
+        summary: 'Campos obrigatorios!',
+        duration: 2000,
+      });
+    }
+
+    this.productService.RegisterProduct(form.value).subscribe(
+      (res)=>{
+        return this.notification.success({
+          detail: 'Sucesso!',
+          summary: 'Produto criado com sucesso!',
+          duration: 2000,
+        });
+      },
+      (err)=>{
+        return this.notification.error({
+          detail: 'Erro!',
+          summary: 'Erro ao criar Produto!',
+          duration: 2000,
+        });
+      }
+    )
   }
 
 }
