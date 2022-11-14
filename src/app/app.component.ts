6cb4b4fc5d5ca04889b42746ad1f0d0a62eba898
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 16:42:41 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+
+  constructor(private toast: NgToastService){}
+
+
+  showSuccess() {
+    this.toast.success({detail:"SUCCESS",summary:'Your Success Message'});
+  }
+  
+  showError() {
+    this.toast.error({detail:"ERROR",summary:'Your Error Message',sticky:true});
+  }
+
+  showInfo() {
+    this.toast.info({detail:"INFO",summary:'Your Info Message',sticky:true});
+  }
+
+  showWarn() {
+    this.toast.warning({detail:"WARN",summary:'Your Warn Message'});
+  }
 }
