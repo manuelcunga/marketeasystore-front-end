7954c0c1749c768d33080ea82100fc45c4a0f731
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 16:42:32 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,51 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CreateOrdersComponent } from './create-orders/create-orders.component';
-import { ListOrdersComponent } from './list-orders/list-orders.component';
-import { ListProductsComponent } from './list-products/list-products.component';
-import { LoginComponent } from './login/login.component';
-import { ProductComponent } from './product/product.component';
-import { RegisterComponent } from './register/register.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CreateOrdersComponent } from "./create-orders/create-orders.component";
+import { ListOrdersComponent } from "./list-orders/list-orders.component";
+import { ListProductsComponent } from "./list-products/list-products.component";
+import { LoginComponent } from "./login/login.component";
+import { ProductComponent } from "./product/product.component";
+import { RegisterComponent } from "./register/register.component";
+import { UserGuard } from "./services/auth/user.guard";
 
 const routes: Routes = [
   {
-    path: '',
-    component: LoginComponent
+    path: "",
+    component: LoginComponent,
   },
   {
-    path: 'register',
-    component: RegisterComponent
+    path: "register",
+    component: RegisterComponent,
   },
   {
-    path: 'produto',
-    component: ProductComponent
+    path: "produto",
+    component: ProductComponent,
+    canActivate: [UserGuard],
   },
   {
-    path: 'produtos',
-    component: ListProductsComponent
+    path: "produtos",
+    component: ListProductsComponent,
+    canActivate: [UserGuard],
   },
   {
-    path: 'criar-pedidos',
-    component: CreateOrdersComponent
+    path: "criar-pedidos",
+    component: CreateOrdersComponent,
+    canActivate: [UserGuard],
   },
   {
-    path: 'list-pedidos',
-    component: ListOrdersComponent
-  }
+    path: "list-pedidos",
+    component: ListOrdersComponent,
+    canActivate: [UserGuard],
+  },
+  {
+    path: "criar-produtos",
+    component: ListOrdersComponent,
+    canActivate: [UserGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
