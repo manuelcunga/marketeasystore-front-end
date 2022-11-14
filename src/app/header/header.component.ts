ed3a37a0bcdf069b3cd2ace7fcc74e800833c38a
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 16:43:00 +0100

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  
 
+  logout(){
+    localStorage.removeItem('user_token')
+    sessionStorage.removeItem('user_session')
+  }
 }
