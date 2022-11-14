dd65473f56d000c516912694feccef3e20e1453d
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-11-14 16:42:46 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { NgToastModule } from 'ng-angular-popup';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,6 +11,7 @@ import { ProductComponent } from './product/product.component';
 import { ListProductsComponent } from './list-products/list-products.component';
 import { CreateOrdersComponent } from './create-orders/create-orders.component';
 import { ListOrdersComponent } from './list-orders/list-orders.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,14 @@ import { ListOrdersComponent } from './list-orders/list-orders.component';
     ProductComponent,
     ListProductsComponent,
     CreateOrdersComponent,
-    ListOrdersComponent
+    ListOrdersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
