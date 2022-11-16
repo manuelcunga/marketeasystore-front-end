c3a03332ff5c2db7132d8a62efd0b0850ccf07d3
manuelcunga <manuelvianacunga@gmail.com>
2022-11-16 14:08:35 +0100

    add orders service

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import {  Observable, throwError } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 import { RegisterDTO } from './dtos/register.dto';
 import { environment } from 'src/environments/environment';
 import { CreateOrderDTO } from './dtos/createOrder.dto';
+import { OrderResponse } from './dtos/ordersReponse';
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +15,50 @@ export class OrdersService {
   apiUrl: string = environment.apiUrl;
 
   constructor(private httpClient: HttpClient){ }
-
   createOrder(data: CreateOrderDTO): Observable<any>{
-     return this.httpClient.post(`${this.apiUrl}/order`, data).pipe(
-      catchError(this.handleError)
+    const token = localStorage.getItem("user_token");
+    const httpOption = new HttpHeaders().set(
+      "Authorization",
+      "bearer " + token
     );
+
+    return this.httpClient
+      .post(`${this.apiUrl}/order`, data, {
+        headers: httpOption,
+      })
+      .pipe(catchError(this.handleError));
   }
+
+  ListALlOrders(): Observable<OrderResponse[]> {
+    const token = localStorage.getItem("user_token");
+    const httpOption = new HttpHeaders().append(
+      "Authorization",
+      "bearer " + token,
+
+    );
+    
+    return this.httpClient.get<OrderResponse[]>(`${this.apiUrl}/orders`, { headers: httpOption, }
+    ).pipe(
+      map((obj)=> obj),
+      catchError(e => this.handleError(e))
+    )
+  }
+
+  DeleteOrder(id: string): Observable<OrderResponse[]> {
+    const token = localStorage.getItem("user_token");
+    const httpOption = new HttpHeaders().append(
+      "Authorization",
+      "bearer " + token,
+
+    );
+    
+    return this.httpClient.delete<OrderResponse[]>(`${this.apiUrl}/order/${id}`, { headers: httpOption, }
+    ).pipe(
+      map((obj)=> obj),
+      catchError(e => this.handleError(e))
+    )
+  }
+
 
   handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
