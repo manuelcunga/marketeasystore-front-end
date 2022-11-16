29b84a6ae39551401a1bee446cbd52b27ed88088
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-11-16 10:24:03 +0100

    create order component ts

--- src/app/create-orders/create-orders.component.ts
+++ src/app/create-orders/create-orders.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NgToastService } from 'ng-angular-popup';
+import { OrdersService } from '../services/orders.service';
 
 @Component({
   selector: 'app-create-orders',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private orderService: OrdersService,  private notification: NgToastService) { }
 
   ngOnInit(): void {
+  }
+
+  CreateOrder(form: NgForm){
+    console.log(form.value)
+      this.orderService.createOrder(form.value)
+      .subscribe((data)=> {
+        return this.notification.success({
+          detail: 'Sucesso!',
+          summary: 'Pedido criado com sucesso!',
+          duration: 2000,
+        });
+      } , (err)=>{
+        return this.notification.error({
+          detail: 'Erro!',
+          summary: 'Erro ao criar Pedido!',
+          duration: 2000,
+        });
+      })
   }
 
 }
